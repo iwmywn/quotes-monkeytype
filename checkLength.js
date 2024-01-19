476aacabbbcf336cfdf03daf6e9ad3b489e6851e
Anh Tuan <thongocnguyen1011@gmail.com>
2024-01-19 07:23:25 +0700

    update checkLength

--- checkLength.js
+++ checkLength.js
@@ -1,13 +1,9 @@
 const quotes = [
-  {
-    "text": "example",
-    "source": "example",
-    "length": 7,
-    "id": 1
-  }
+  
 ];
 
 const incorrectLengthIds = [];
+let idCheck = false;
 
 quotes.forEach((quote, index) => {
   if (quote.length !== quote.text.length) {
@@ -15,12 +11,18 @@ quotes.forEach((quote, index) => {
   }
 
   if (index > 0 && quote.id !== quotes[index - 1].id + 1) {
+    if (!idCheck)
+      idCheck = true;
     console.log(`Warning: Ids are not in ascending order. Current id: ${quote.id}, Previous id: ${quotes[index - 1].id}`);
-}
+  }
 });
 
 if (incorrectLengthIds.length > 0) {
   console.log("Ids with incorrect length:", incorrectLengthIds);
 } else {
   console.log("All lengths are correct.");
+}
+
+if (!idCheck) {
+  console.log("All ids are correct.");
 }
